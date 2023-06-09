32721aa947f3bfb4927d48e77bdfe282f2505499
Cherrying55 <cherrying556@protonmail.com>
2023-06-09 20:28:30 -0300

    Gustavo merge sprint 2

--- src/controllers/authorization.controller.js
+++ src/controllers/authorization.controller.js
@@ -31,6 +31,11 @@ export async function signin(req, res) {
 
 export async function getUserbyId(req,res){
     const id = parseInt(req.params.id);
+    let {page} = req.headers;
+
+    if(!page){
+        page = 1;
+    }
   
     if (!id) return res.sendStatus(404);
     
@@ -50,7 +55,7 @@ export async function getUserbyId(req,res){
         const obj={
             name: query.rows[0].name,
             foto: query.rows[0].foto,
-            posts:query2.rows
+            posts:query2.rows.slice(0,10*page)
         }
         return res.send(obj)
         
@@ -64,16 +69,71 @@ export async function getUserbyId(req,res){
 
 export async function getUsers(req,res){
 
-    const {search} = req.headers;
-    
+    const search = req.headers.search;
+    const authorization = req.headers.authorization;
+    const token = authorization?.replace('Bearer ', '');
+
+    if(!authorization){
+        return res.sendStatus(401)
+    }
+    console.log("search: " + search);
 
     
 
     try {
+        const session = await db.query(`
+         SELECT * FROM login WHERE token=$1
+        `,[token]);
+        if(session.rows.length === 0){
+            return  res.sendStatus(401)
+        }
+        console.log(session.rows);
+        const user = await db.query(`
+        SELECT * FROM users WHERE id=$1
+        `,[session.rows[0].idUser]);
+        const userid = user.rows[0].id;
+        console.log("userid: " + userid)
+
         const query = await db.query(`
-        SELECT * FROM users WHERE name LIKE '%' || $1 || '%'
+        SELECT * 
+        FROM users 
+        WHERE name LIKE '%' || $1 || '%'
+        LIMIT 4
         `, [search]);
-        return res.send(query.rows.slice(0,4))
+        console.log("like: ");
+        console.log(query.rows);
+        const busca = query.rows;
+        const segue = [];
+        for(const i of busca){
+            const pegarid = await db.query(`
+            SELECT * FROM users
+            WHERE name=$1
+            `, [i.name]);
+            console.log("Id: ")
+            console.log(pegarid.rows[0].id);
+            
+            const conferir = await db.query(`
+            SELECT * FROM follows
+            WHERE "followerId"=$1 AND "followedId"=$2
+            `, [pegarid.rows[0].id, userid]);
+            console.log(conferir.rows.length);
+            if(conferir.rows.length !== 0){
+                i.follow = true;
+                segue.push(i)
+            }
+            else{
+                i.follow = false;
+            } 
+        };
+        const naosegue = busca.filter((b => !segue.includes(b)));
+        let ar = [];
+        for(const i of segue){
+            ar.push(i)
+        }
+        for(const i of naosegue){
+            ar.push(i)
+        }
+        return res.send(ar);
     } catch (error) {
         return res.sendStatus(500);
     }
